542b2b7d2f1de8331aa4f8607b49e8b197265821
Christinamawel <christinamawel@gmail.com>
2021-10-29 09:11:08 -0700

    passes all tests in README.md

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function BeepBoop(number) {
   const twoRegex = new RegExp("2");
   const threeRegex = new RegExp("3");
 
+  if (number > 1000) {
+    return "Mr. Roboger can't count that high. please type in a smaller number."
+  }
+
   for (i = 0; i <= number; i++) {
     numberArray.push(i.toString())
   }
@@ -21,4 +25,4 @@ function BeepBoop(number) {
   return numberArray.join(", ")
 }
 
-console.log(BeepBoop(23));
+console.log(BeepBoop(1001));
