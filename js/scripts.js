6c754fee5550486306e80b400efd6954b41a551d
Christinamawel <christinamawel@gmail.com>
2021-10-29 12:00:21 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //business Logic
+let lastInput = "";
 
 function BeepBoop(number) {
   let numberArray = [];
@@ -27,7 +28,39 @@ function BeepBoop(number) {
   return numberArray.join(", ")
 }
 
-console.log(BeepBoop(1001));
+function angryBeepBoop(input) {
+  console.log(input);
+  console.log(lastInput);
+  if (input === 69 || input === 666 || input === 420) {
+    if (lastInput === "strikeTwo") {
+      return "no-response"
+    } else if (lastInput === "strikeOne") {
+      return "very-angry"
+    } else {
+      return "angry"
+    }
+  } else {
+    if (lastInput === "strikeTwo") {
+      return "no-response"
+    } else if (lastInput === "strikeOne") {
+      return "forgive"
+    } else {
+      return "normal"
+    }
+  }
+}
+
+function lastInputChanger(output, input) {
+  if (input !== 69 && output === "no-response" || input !== 666 && output === "no-response" || input !== 420 && output === "no-response") {
+    lastInput = "strikeOne"
+  } else if (output === "normal" || output === "forgive") {
+    lastInput = ""
+  } else if (output === "angry") {
+    lastInput = "strikeOne"
+  } else if (output === "very-angry" || output === "no-response") {
+    lastInput = "strikeTwo"
+  }
+}
 
 //UI Logic
 $(document).ready(function() {
@@ -35,30 +68,42 @@ $(document).ready(function() {
     event.preventDefault();
     $(".response").hide();
     $(".robot").hide();
-    const input = $("#number-input").val();
+    const input = parseInt($("#number-input").val());
 
-    if (input == 69 || input == 666 || input == 420) {
+    if (angryBeepBoop(input) === "normal") {
+      if (input < 80) {
+        $("#pleasant").show();
+        $("#normal-robot").show();
+      } else if (input < 200) {
+        $("#opinion").show();
+        $("#normal-robot").show();
+      } else if (input < 500) {
+        $("#kind").show();
+        $("#happy-robot").show();
+      } else if (input < 700) {
+        $("#show").show();
+        $("#normal-robot").show();
+      } else {
+        $("#compliment").show();
+        $("#happy-robot").show();
+      }
+      console.log("ran normal")
+    } else if (angryBeepBoop(input) === "angry") {
       $("#angry").show();
       $("#angry-robot").show();
-    } else if (input < 80) {
-      $("#pleasant").show();
-      $("#normal-robot").show();
-    } else if (input < 200) {
-      $("#opinion").show();
-      $("#normal-robot").show();
-    } else if (input < 500) {
-      $("#kind").show();
-      $("#happy-robot").show();
-    } else if (input < 700) {
-      $("#show").show();
-      $("#normal-robot").show();
+    } else if (angryBeepBoop(input) === "very-angry") {
+      $("#very-angry").show();
+      $("#angry-robot").show();
     } else {
-      $("#compliment").show();
-      $("#happy-robot").show();
+      $("#no-response").show();
+      $("#angry-robot").show();
     }
 
+    if (angryBeepBoop(input) !== "no-response") {
     $("#output").text(BeepBoop(input));
+    }
     $("#output-container").show();
     $("#intro").hide();
+    lastInputChanger(angryBeepBoop(input), input);
   })
 })
