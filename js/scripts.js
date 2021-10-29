cfb737e376076c8aca09f9bb15f712b92b52329a
Christinamawel <christinamawel@gmail.com>
2021-10-29 08:56:47 -0700

    pass seccond test in README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function BeepBoop() {
-  return 0
+function BeepBoop(number) {
+  let numberArray = []
+  const oneRegex = new RegExp("1")
+  for (i = 0; i <= number; i++) {
+    numberArray.push(i.toString())
+  }
+  console.log(numberArray)
+  numberArray.forEach(function(num, index) {
+    if (num.match(oneRegex)) {
+      numberArray[index] = "Beep!"
+    }
+  })
+  return numberArray.join(", ")
 }
+
+console.log(BeepBoop(5));
