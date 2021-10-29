a1c8b30c4f118f03084b12796d664ae86399a6e7
Christinamawel <christinamawel@gmail.com>
2021-10-29 09:01:34 -0700

    pass the third  test in README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 function BeepBoop(number) {
-  let numberArray = []
-  const oneRegex = new RegExp("1")
+  let numberArray = [];
+  const oneRegex = new RegExp("1");
+  const twoRegex = new RegExp("2");
+
   for (i = 0; i <= number; i++) {
     numberArray.push(i.toString())
   }
-  console.log(numberArray)
+
   numberArray.forEach(function(num, index) {
-    if (num.match(oneRegex)) {
-      numberArray[index] = "Beep!"
+    if (num.match(twoRegex)) {
+      numberArray[index] = "Boop";
+    } else if (num.match(oneRegex)) {
+      numberArray[index] = "Beep!";
     }
   })
+
   return numberArray.join(", ")
 }
 
